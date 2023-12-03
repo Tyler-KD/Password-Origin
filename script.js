7ea4f05bacccf0d0782e5c42b715af8d38124c3c
unknown <tyler.kd.knapp@gmail.com>
2023-12-03 17:10:38 -0600

    Updated user password length criteria

--- script.js
+++ script.js
@@ -1,35 +1,49 @@
 // Assignment code here
 // generateBtn = document.querySelector("#generate");
 
+// An array stores a collection of multiple items under a single variable name
 // Arrays for numbers, uppercase, lowercase, and special characters
-
+// Uppercase array
 const cap = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+// Lowercase array
 const lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// Numbers array
 const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+// Special characters array
 const special = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", ">", "=", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~", "'"];
+// Password String array for displaying password to user
 const passString = [];
 
-var userChoiceLength = function LengthPrompt() {
-  var userChoiceLengthInput = Number(prompt("How many characters would you like your password to be?\nPlease enter a length between 8 and 128 characters"));
 
-  if (userChoiceLengthInput === 0) {
-    window.alert("How many characters would you like your password to be?\nPlease enter a length between 8 and 128 characters")
-    LengthPrompt();
+// Accept user password criteria with prompts
+function userChoiceLength() {
+  // While loop repeats initial code block as long as the condition evaluates to true
+  while (true) {
+    // Prompt () takes length of the password
+    userChoiceLengthInput = prompt('Please enter a password length between at least 8 characters and no more than 128 characters.');
+    // userChoiceLengthInput == null means the user's password length has no value
+    if (userChoiceLengthInput === null) {
+      // Alerts wrapped in if-statements for all other options the user wants
+      window.alert('Your password must contain at least 8 characters and no more than 128 characters.');
+      return true;
+    } else {
+      // userChoiceLengthInput < 8 means the user's password length is less than 8 characters
+      // userChoiceLengthInput > 128 means the user's password length is longer than 128 characters
+      // isNaN(userChoiceLengthInput) means the user's password length is not a number
+      // userChoiceLengthInput == null means the user's password length has no value
+      if (userChoiceLengthInput < 8 || userChoiceLengthInput > 128 || isNaN(userChoiceLengthInput) || userChoiceLengthInput == null) {
+        window.alert('Your password must contain at least 8 characters and no more than 128 characters.');
+      } else {
+        return parseInt(userChoiceLengthInput);
+      }
+    }
   }
-  if (userChoiceLengthInput < 8) {
-    window.alert("Your password must contain more than 8 characters");
-    LengthPrompt();
-  }
-  if (userChoiceLengthInput > 128) {
-    window.alert("Your password must contain less than 128 characters");
-    LengthPrompt();
-  }
-  else {
-    return userChoiceLengthInput;
-  };
 };
 
+// Verify user's criteria
 function UpperPrompt() {
+  // Confirm allows option to enter "No" with Cancel Button
+  // If the user clicks "Okay," the box returns true.  If the user clicks "Cancel," the box returns false.
   return confirm("Would you like to include upper case letters?");
 };
 
@@ -45,29 +59,19 @@ function SpecialPrompt() {
   return confirm("Would you like to include special characters?");
 };
 
-// generatePassword function
+// generatePassword function based on user's criteria
 function generatePassword() {
-
-  // accept user input (prompt)
-
 
   var passwordSize = userChoiceLength();
   console.log (passwordSize);
-  // console.log("Password length: " + userChoiceLength);
-  // userChoiceUpper();
-  // console.log("Uppercase letters: " + userChoiceUpper);
-  // userChoiceLower();
-  // console.log("Lowercase letters: " + userChoiceLower);
-  // userChoiceNumber();
-  // console.log("Numbers: " + userChoiceNumber);
-  // userChoiceSpecialCharacters();
-  // console.log("Special Characters: " + userChoiceSpecialCharacters);
 
-  // Initialize an empty array for character sets
+  // Initialize an empty array for character sets based on the user's criteria
 
   var possibleCharacters = [];
 
-
+  // The concatenation method joins 2 or more arrays and returns new array
+  // Concat user's criteria to possibleCharacters array
+  // Each user criteria array is concatenated as long as it is confirmed 
   if (UpperPrompt() === true) {
     possibleCharacters = possibleCharacters.concat(cap);
   };
@@ -87,25 +91,34 @@ function generatePassword() {
     possibleCharacters = possibleCharacters.concat(special);
   };
 
+  // Convert password String array into a string
+  // getRandom function returns the amount of characters in possibleCharacters array through for-loops
+  // For-loops execute code more than once
+  // (i=0) means the index starts at 0 integer
+  // (i < passwordSize) means the for-loop will run as long as the index number is less than the length of passwordSize function expression
+  // (i++) is the incremental increase for each index integer
   var passString = ""
   for (i=0; i < passwordSize; i++) {
+    // This statement will run each time the loop is executed
     passString = passString + getRandom (possibleCharacters);
   }
  
+  // Display password to the page
   return passString;
 }
 
+// Random function returns a random number for possibleCharacters array
+// getRandom returns one character at a time
 function getRandom(arr) {
+  // Math.random only runs between 0 and 1
+  // Math.floor rounds down to a whole number
+  // Math.floor(Math.random) multiplies by arrary.length for the total number of potential characters possible
   var randIndex = Math.floor(Math.random() * arr.length);
   console.log(randIndex);
   var randElement = arr[randIndex];
   console.log(randElement);
   return randElement;
 };
-
-
-
-// generatePassword ();
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
