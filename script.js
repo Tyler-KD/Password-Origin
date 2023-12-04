fda582f3bcf41d0917502a67e2b9485c5b63fca3
unknown <tyler.kd.knapp@gmail.com>
2023-12-03 18:20:10 -0600

    Reformatted document

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 
 // An array stores a collection of multiple items under a single variable name
 // Arrays for numbers, uppercase, lowercase, and special characters
+// The constant declaration declares block-scoped local variables. The value of a constant cannot be reassigned.
 // Uppercase array
 const cap = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 // Lowercase array
@@ -12,10 +13,12 @@ const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 // Special characters array
 const special = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", ">", "=", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~", "'"];
 // Password String array for displaying password to user
+// Stores all of the generated random characters
 const passString = [];
 
 
 // Accept user password criteria with prompts
+
 function userChoiceLength() {
   // While loop repeats initial code block as long as the condition evaluates to true
   while (true) {
@@ -34,13 +37,15 @@ function userChoiceLength() {
       if (userChoiceLengthInput < 8 || userChoiceLengthInput > 128 || isNaN(userChoiceLengthInput) || userChoiceLengthInput == null) {
         window.alert('Your password must contain at least 8 characters and no more than 128 characters.');
       } else {
-        return parseInt(userChoiceLengthInput);
+        return userChoiceLengthInput;
       }
     }
   }
 };
 
 // Verify user's criteria
+// At least one character type must be confirmed
+
 function UpperPrompt() {
   // Confirm allows option to enter "No" with Cancel Button
   // If the user clicks "Okay," the box returns true.  If the user clicks "Cancel," the box returns false.
@@ -59,11 +64,12 @@ function SpecialPrompt() {
   return confirm("Would you like to include special characters?");
 };
 
-// generatePassword function based on user's criteria
+// generatePassword function creates the password based on user's criteria
+
 function generatePassword() {
 
   var passwordSize = userChoiceLength();
-  console.log (passwordSize);
+  console.log(passwordSize);
 
   // Initialize an empty array for character sets based on the user's criteria
 
@@ -91,27 +97,28 @@ function generatePassword() {
     possibleCharacters = possibleCharacters.concat(special);
   };
 
-  // Convert password String array into a string
+  // Convert passString array into a string
   // getRandom function returns the amount of characters in possibleCharacters array through for-loops
   // For-loops execute code more than once
   // (i=0) means the index starts at 0 integer
   // (i < passwordSize) means the for-loop will run as long as the index number is less than the length of passwordSize function expression
   // (i++) is the incremental increase for each index integer
   var passString = ""
-  for (i=0; i < passwordSize; i++) {
+  for (i = 0; i < passwordSize; i++) {
     // This statement will run each time the loop is executed
-    passString = passString + getRandom (possibleCharacters);
+    passString = passString + getRandom(possibleCharacters);
   }
- 
+
   // Display password to the page
   return passString;
 }
 
-// Random function returns a random number for possibleCharacters array
+// getRandom function returns a random number for possibleCharacters array
+
 // getRandom returns one character at a time
 function getRandom(arr) {
-  // Math.random only runs between 0 and 1
-  // Math.floor rounds down to a whole number
+  // Math.random method returns a random number from 0 (inclusive) up to but not including 1 (exclusive)
+  // Math.floor method rounds a number DOWN to the nearest integer
   // Math.floor(Math.random) multiplies by arrary.length for the total number of potential characters possible
   var randIndex = Math.floor(Math.random() * arr.length);
   console.log(randIndex);
